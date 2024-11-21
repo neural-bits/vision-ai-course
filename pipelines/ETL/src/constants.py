f7ec85c1e8155d70b568c82c3eaf320663621ea5
Joywalker <alexandrurazvant@gmail.com>
2024-11-21 23:54:44 +0200

    feat(data): Add pexels data pipeline testing

---
+++ pipelines/ETL/src/constants.py
@@ -0,0 +1,4 @@
+UNSPLASH_API_URL = "https://api.unsplash.com/search/photos?query={topic}"
+PEXELS_API_URL = "https://api.pexels.com/v1/search?query={topic}"
+
+__all__ = ["UNSPLASH_API", "PEXELS_API"]
