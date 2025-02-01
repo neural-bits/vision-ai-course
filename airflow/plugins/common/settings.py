53585f8793628bee84978426fffba917075d73e6
Joywalker <alexandrurazvant@gmail.com>
2025-02-01 19:49:37 +0200

    fix(data): Detach airflow, configure plugins and dags

---
+++ airflow/plugins/common/settings.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+
+ROOT_PATH = Path(__file__).parent.parent
+
+
+class APIScrappingSettings(BaseSettings):
+    unsplash_api_key: str
+    pexels_api_key: str
+
+    api_image_subject: str
+    api_image_last_page: int
+    api_image_num_pages: int
+    api_image_num_items_per_page: int
+
+
+class MongoSettings(BaseSettings):
+    mongo_uri: str
+    mongo_db_name: str
+    raw_data_collection: str
+    clean_data_collection: str
+    dataset_collection: str
+
+
+class S3Settings(BaseSettings):
+    aws_s3_datalake_bucket: str
+    aws_s3_datawarehouse_bucket: str
+    aws_access_key_id: str
+    aws_secret_access_key: str
+
+
+def get_mongo_settings():
+    return MongoSettings(_env_file=ROOT_PATH / ".secrets/.env-db.env")
+
+
+def get_s3_settings():
+    return S3Settings(_env_file=ROOT_PATH / ".secrets/.env-storage.env")
+
+
+def get_api_scrapping_settings():
+    return APIScrappingSettings(_env_file=ROOT_PATH / ".secrets/.env-scrapping.env")
